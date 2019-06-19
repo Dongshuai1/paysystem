1465ce4c477f8178a229360bf8aebed9fa974000
dongshuai1 <836682526@qq.com>
2019-06-19 15:54:44 +0800

    first commit

---
+++ src/main/java/com/java1234/repository/OrderRepository.java
@@ -0,0 +1,18 @@
+package com.java1234.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import com.java1234.entity.Order;
+
+/**
+ * 订单Repository接口
+ * @author Administrator
+ *
+ */
+public interface OrderRepository extends JpaRepository<Order, Integer>,JpaSpecificationExecutor<Order>{
+
+	@Query(value="select * from t_order where order_no=?1",nativeQuery=true)
+	public Order getByOrderNo(String orderNo);
+}
